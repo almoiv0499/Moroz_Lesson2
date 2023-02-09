b10cd39b13ec46b14e26f242ea8bfffcd5ed481c
Aleksandr Moroz <morozaleksandriv@gmail.com>
2023-02-10 00:18:54 +0300

    feature created navigation in MainActivity.kt, added Buttons and TextView for activity_main.xml

--- app/src/main/java/ru/aston/moroz_lesson2/MainActivity.kt
+++ app/src/main/java/ru/aston/moroz_lesson2/MainActivity.kt
@@ -1,11 +1,34 @@
 package ru.aston.moroz_lesson2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.aston.moroz_lesson2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navigateToSecondFragment()
+    }
+
+    fun changeViewsLanguage() {
+        binding.buttonEnglishLanguage.setOnClickListener {
+
+        }
+        binding.buttonRussianLanguage.setOnClickListener {
+
+        }
+    }
+
+    private fun navigateToSecondFragment() {
+        binding.buttonNavigateToSecondActivity.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/ru/aston/moroz_lesson2/SecondActivity.kt
@@ -0,0 +1,13 @@
+package ru.aston.moroz_lesson2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+}
