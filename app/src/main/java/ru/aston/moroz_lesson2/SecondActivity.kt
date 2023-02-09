5a31d3d53903934bfa7cf3bc1900fdcc89c6e0ce
Aleksandr Moroz <morozaleksandriv@gmail.com>
2023-02-10 00:30:11 +0300

    feature added Button and InputEditText in activity_second.xml

--- app/src/main/java/ru/aston/moroz_lesson2/SecondActivity.kt
+++ app/src/main/java/ru/aston/moroz_lesson2/SecondActivity.kt
@@ -1,13 +1,33 @@
 package ru.aston.moroz_lesson2
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import ru.aston.moroz_lesson2.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+    companion object {
+        private const val SAVE_STATE_KEY = "save_state_key"
     }
 
+    private lateinit var binding: ActivitySecondBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navigateToMainActivity()
+    }
+
+    private fun navigateToMainActivity() {
+        binding.buttonNavigateToMainActivity.setOnClickListener {
+
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        val valueFromEditText = binding.inputText.text.toString()
+        outState.putString(SAVE_STATE_KEY, valueFromEditText)
+    }
 }
