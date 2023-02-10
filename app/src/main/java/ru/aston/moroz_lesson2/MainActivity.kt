92fffcce46b304e850737b11a1a2adf5909d0b6c
Aleksandr Moroz <morozaleksandriv@gmail.com>
2023-02-10 18:35:35 +0300

    feature added SharedPrefs in MainActivity.kt, saved state in SecondActivity.kt

--- app/src/main/java/ru/aston/moroz_lesson2/MainActivity.kt
+++ app/src/main/java/ru/aston/moroz_lesson2/MainActivity.kt
@@ -1,27 +1,44 @@
 package ru.aston.moroz_lesson2
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.aston.moroz_lesson2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val STORE_KEY = "store_key"
+        private const val STORAGE_FILE = "storage_file"
+    }
 
     private lateinit var binding: ActivityMainBinding
+
+    private val sharedPreferences by lazy(LazyThreadSafetyMode.NONE) {
+        getSharedPreferences(STORAGE_FILE, Context.MODE_PRIVATE)
+    }
+
+    private val editor by lazy(LazyThreadSafetyMode.NONE) {
+        sharedPreferences.edit()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        storeAndShowGotValue()
         navigateToSecondFragment()
     }
 
-    fun changeViewsLanguage() {
-        binding.buttonEnglishLanguage.setOnClickListener {
-
-        }
-        binding.buttonRussianLanguage.setOnClickListener {
-
+    private fun storeAndShowGotValue() {
+        val receivedValue = intent.getStringExtra(SecondActivity.NAVIGATE_KEY)
+        if (receivedValue != null && receivedValue.isNotEmpty()) {
+            editor.putString(STORE_KEY, receivedValue).apply()
+            binding.textViewMainActivity.text = receivedValue
+        } else {
+            binding.textViewMainActivity.text =
+                sharedPreferences.getString(STORE_KEY, getString(R.string.hello_main_activity))
         }
     }
 
@@ -30,5 +47,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        editor.remove(STORE_KEY).apply()
     }
 }
--- app/src/main/java/ru/aston/moroz_lesson2/SecondActivity.kt
+++ app/src/main/java/ru/aston/moroz_lesson2/SecondActivity.kt
@@ -1,12 +1,13 @@
 package ru.aston.moroz_lesson2
 
+import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.aston.moroz_lesson2.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
     companion object {
+        const val NAVIGATE_KEY = "navigate_key"
         private const val SAVE_STATE_KEY = "save_state_key"
     }
 
@@ -16,18 +17,40 @@ class SecondActivity : AppCompatActivity() {
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        savedInstanceState?.getString(SAVE_STATE_KEY)?.let { enteredValue ->
+            with(binding) {
+                textViewForSaveInstance.text = enteredValue
+                inputText.setText(enteredValue)
+            }
+        }
+
+        setValueToTextView()
         navigateToMainActivity()
     }
 
     private fun navigateToMainActivity() {
-        binding.buttonNavigateToMainActivity.setOnClickListener {
-
+        with(binding) {
+            buttonNavigateToMainActivity.setOnClickListener {
+                val enteredValue = inputText.text.toString()
+                val intent = Intent(this@SecondActivity, MainActivity::class.java)
+                intent.putExtra(NAVIGATE_KEY, enteredValue)
+                startActivity(intent)
+            }
         }
     }
 
-    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
-        super.onSaveInstanceState(outState, outPersistentState)
-        val valueFromEditText = binding.inputText.text.toString()
-        outState.putString(SAVE_STATE_KEY, valueFromEditText)
+    private fun setValueToTextView() {
+        with(binding) {
+            inputText.setOnClickListener {
+                val enteredValue = inputText.text.toString()
+                textViewForSaveInstance.text = enteredValue
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val enteredValue = binding.inputText.text.toString()
+        outState.putString(SAVE_STATE_KEY, enteredValue)
     }
 }
